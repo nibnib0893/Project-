9acce156b7900222159916ab4524a895ea1a4f61
nibnib0893 <83099418+nibnib0893@users.noreply.github.com>
2022-06-25 13:07:24 -0400

    Add files via upload

---
+++ projet.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+
+function KeyIsDown(){
+  if (KeyIsDown(DOWN_ARROW)){
+    background('red')
+    }
+}
+
+function draw() 
+{
+  KeyIsDown();
+  background(30);
+
+}
+
+
+
+
